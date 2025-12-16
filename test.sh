a37a8e95aa584b6444eb49263be746da79c59345
james <jdmichaelryan@gmail.com>
2025-12-16 22:42:37 +0000

    fix: final implementation of displaying pkgs

--- test.sh
+++ test.sh
@@ -3,16 +3,8 @@
 
 date=$(date "+Start-Date: %Y-%m-%d")
 
-test="is"
-
-if [ -e "prac.txt" ]; then
-    echo "naur"
-else
-    cat "/var/log/apt/history.log" >> prac.txt
-fi
-format="Start-Date: 2025-12-01"
-echo $(sed -n "/Start-Date: 2025-12-01/,$p" prac.txt) # -n allows you to control which lines are printed, $ marks the end and p is to print
-
 while read -r line || [[ -n "$line" ]]; do
-    echo "$line"
-done < "test.txt"
+    if [[ "$line" =~ "Commandline:" ]]; then
+        echo "$line" >> test1.txt
+    fi
+done < "/var/log/apt/history.log"
