8bb288dc19bc21bd024e3f7d0a058c71f8d202f0
james <jdmichaelryan@gmail.com>
2025-12-16 22:56:06 +0000

    func: rudimentary implementation - will tidy up later

--- main.sh
+++ main.sh
@@ -1,10 +1,20 @@
 #!/bin/env bash
 function display_pkg ()
-{
-	log=$(cat "/var/log/apt/history.log") # () used to run a command
-	current_date=$(date "+Start-Date: %Y-%m-%d")
-	cat "$log" | grep "^$current_date"
+{	
+	# i will add this feature at a later date:
+	log=$"/var/log/apt/history.log" # () used to run a command
+	# current_date=$(date "+Start-Date: %Y-%m-%d")
+	# cat "$log" | grep "^$current_date"
 
+	while read -r line || [[ -n "$line" ]]; do
+		if [[ "$line" =~ "Commandline: /usr/bin/apt install" ]]; then
+			echo "$line" >> "display.txt"
+		fi
+	done < "$log"
+	
+	printf "%s\n" "$(cat "display.txt")"
+
+	rm "display.txt"
 }
 
 PS3="Please enter a choice: " # the PS3 variable name is the prompt for the select statement
@@ -14,7 +24,7 @@ do
 	case $opt in
 		"recently installed packages")
 
-		echo "${log:current_date}"
+		display_pkg
 		;;
 
 		"delete packages")
