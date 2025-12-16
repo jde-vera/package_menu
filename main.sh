b033216bec7287442b0fda094daf648fbe7ac896
james <jdmichaelryan@gmail.com>
2025-12-16 22:28:37 +0000

    func: rudimentary implementation of viewing installed pkgs

--- main.sh
+++ main.sh
@@ -1,4 +1,11 @@
 #!/bin/env bash
+function display_pkg ()
+{
+	log=$(cat "/var/log/apt/history.log") # () used to run a command
+	current_date=$(date "+Start-Date: %Y-%m-%d")
+	cat "$log" | grep "^$current_date"
+
+}
 
 PS3="Please enter a choice: " # the PS3 variable name is the prompt for the select statement
 options=("recently installed packages" "delete packages" "quit")
@@ -6,8 +13,7 @@ select opt in "${options[@]}"
 do
 	case $opt in
 		"recently installed packages")
-		log=$(cat "/var/log/apt/history.log") # () used to run a command
-	       	current_date=$(date "+Start-Date: %Y-%m-%d")	
+
 		echo "${log:current_date}"
 		;;
 
