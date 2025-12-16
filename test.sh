c56b12ab46a421b0ce0e60af87341ad12b7518e7
james <jdmichaelryan@gmail.com>
2025-12-16 22:15:34 +0000

    fix: kind of fixed the logic

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/env bash
 # this is a test for new commands i'm unfamiliar with
 
-date "+Start-Date: %Y-%m-%d"
+date=$(date "+Start-Date: %Y-%m-%d")
 
 test="is"
 
-cat "/var/log/apt/history.log" | grep "^$test" test.txt
+if [ -e "prac.txt" ]; then
+    echo "naur"
+else
+    cat "/var/log/apt/history.log" >> prac.txt
+fi
+format="Start-Date: 2025-12-01"
+echo $(sed -n "/Start-Date: 2025-12-01/,$p" prac.txt) # -n allows you to controll which lines are printed, $ marks the end and p is to print
