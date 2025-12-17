7c370330245982de02fd7cbc40e03c57e6cd47c5
james <jdmichaelryan@gmail.com>
2025-12-17 00:47:03 +0000

    chore: add test dir for readability

---
+++ test/test.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+# this is a test for new commands i'm unfamiliar with
+
+date=$(date "+Start-Date: %Y-%m-%d")
+
+while read -r line || [[ -n "$line" ]]; do
+    if [[ "$line" =~ "Commandline:" ]]; then
+        echo "$line" >> test1.txt
+    fi
+done < "/var/log/apt/history.log"
+
+if [[ -e "user_os.txt" ]]; then
+    :
+else
+    cat "/etc/os-release" >> "user_os.txt"
+fi
+
+os_check=$(grep '^NAME=' "user_os.txt")
+
+if [[ ${os_check,,} =~ "mint" ]]; then # recap $ uses the value and {} is parameter expansion. bash 4+ introduced case modification operators such as ,,
+    echo "works!"
+fi
